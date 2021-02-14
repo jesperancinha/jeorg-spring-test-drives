cfdaabd0454c0a7d027908752bb4eb0b2830a74b
Joao Esperancinha <jofisaes@gmail.com>
2021-02-14 18:11:40 +0100

    Spring 5 - Consolerizer first usage

--- spring-5/spring-topics/spring-topic-container/src/main/java/org/jesperancinha/std/topic/container/ContainerLauncher.java
+++ spring-5/spring-topics/spring-topic-container/src/main/java/org/jesperancinha/std/topic/container/ContainerLauncher.java
@@ -3,9 +3,14 @@ package org.jesperancinha.std.topic.container;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.BRIGHT_MAGENTA;
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.GREEN;
+
 @SpringBootApplication
 public class ContainerLauncher {
     public static void main(String[] args) {
+        BRIGHT_MAGENTA.printGenericTitleLn("Service Started");
+        GREEN.printGenericTitleLn("Spring Boot");
         SpringApplication.run(ContainerLauncher.class, args);
     }
 }
