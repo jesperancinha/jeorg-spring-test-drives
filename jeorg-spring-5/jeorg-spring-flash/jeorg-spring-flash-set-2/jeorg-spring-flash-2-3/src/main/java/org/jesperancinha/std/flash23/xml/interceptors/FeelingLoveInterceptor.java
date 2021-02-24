58c71d7a4e45d2669aaefeb90724637f11ebc982
Joao Esperancinha <jofisaes@gmail.com>
2021-02-24 15:40:49 +0100

    🌱 jeorg-spring-flash-2-3 - 1. `mvc:interceptors`, `mvc:interceptor`, `mvc:mapping`, `mvc:exclude-mapping`

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-3/src/main/java/org/jesperancinha/std/flash23/xml/interceptors/FeelingLoveInterceptor.java
@@ -0,0 +1,36 @@
+package org.jesperancinha.std.flash23.xml.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.CYAN;
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.GREEN;
+
+public class FeelingLoveInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+
+        CYAN.printGenericTitleLn("preHandle called!");
+        GREEN.printGenericLn("Drinkin' fine wine");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        CYAN.printGenericTitleLn("postHandle called!");
+        GREEN.printGenericLn("If you like the finer things in life");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception exception) {
+        CYAN.printGenericTitleLn("afterCompletion called!");
+        GREEN.printGenericLn("Tastes like fine wine");
+    }
+}
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-3/src/main/java/org/jesperancinha/std/flash23/xml/interceptors/SpringFlash23Launcher.java
@@ -0,0 +1,19 @@
+package org.jesperancinha.std.flash23.xml.interceptors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringFlash23Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlash23Launcher.class, args);
+    }
+
+    @GetMapping("/")
+    public String getString(){
+        return "Fine Wine";
+    }
+}
