46a2e5746857daad85fb704101a48caa7d36210a
Joao Esperancinha <jofisaes@gmail.com>
2021-02-26 13:10:47 +0100

    jeorg-spring-flash-2-11, 2-12 - 1. `log4j2.xml` `logback.xml`

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-11/src/main/java/org/jesperancinha/std/flash211/logs/SpringFlash211Launcher.java
@@ -0,0 +1,11 @@
+package org.jesperancinha.std.flash211.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlash211Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlash211Launcher.class, args);
+    }
+}
