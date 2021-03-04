5dafec53cc018b56fbf8c2d38a5c6c2b9f1d453d
Joao Esperancinha <jofisaes@gmail.com>
2021-03-04 12:37:00 +0100

    🌱 jeorg-spring-flash-3-16 - 1. `@RequestParameter`, `required`

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-3/jeorg-spring-flash-3-16/src/main/java/org/jesperancinha/std/flash316/requestparam/controller/ThinkController.java
@@ -0,0 +1,36 @@
+package org.jesperancinha.std.flash316.requestparam.controller;
+
+import org.jesperancinha.std.flash316.requestparam.model.Think;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.MAGENTA;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+public class ThinkController {
+
+    @PostMapping(path = "/",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    public Think thinkRandom(
+            @RequestBody
+            final List<String> thinks,
+            @RequestParam(required = false)
+            final String artist) {
+        final List<Think> thinkList = thinks.stream().map(s -> {
+            final Think think1 = new Think();
+            think1.setArtist(artist);
+            think1.setName(s);
+            return think1;
+        }).collect(Collectors.toList());
+        final Think think = thinkList.get((int) (thinkList.size() * Math.random()));
+        MAGENTA.printGenericLn("An artist is not required by default and that we need to specify. This is when we want it to be optional and not mandatory");
+        return think;
+    }
+}
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-3/jeorg-spring-flash-3-16/src/main/java/org/jesperancinha/std/flash316/requestparam/model/Think.java
@@ -0,0 +1,23 @@
+package org.jesperancinha.std.flash316.requestparam.model;
+
+public class Think {
+    private String name;
+
+    private String artist;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
