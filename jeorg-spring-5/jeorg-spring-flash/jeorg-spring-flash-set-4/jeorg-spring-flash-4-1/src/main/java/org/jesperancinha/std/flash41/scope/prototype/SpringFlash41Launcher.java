ac9640176bee4c40778d1022b6012a9517288e8c
Joao Esperancinha <jofisaes@gmail.com>
2021-03-05 12:10:49 +0100

    🌱 jeorg-spring-flash-4-1 - 1. `@Scope`, `prototype`

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-4/jeorg-spring-flash-4-1/src/main/java/org/jesperancinha/std/flash41/scope/prototype/SpringFlash41Launcher.java
@@ -0,0 +1,14 @@
+package org.jesperancinha.std.flash41.scope.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.GREEN;
+
+@SpringBootApplication
+public class SpringFlash41Launcher {
+    public static void main(String[] args) {
+        GREEN.printGenericTitleLn("Service is starting");
+        SpringApplication.run(SpringFlash41Launcher.class, args);
+    }
+}
