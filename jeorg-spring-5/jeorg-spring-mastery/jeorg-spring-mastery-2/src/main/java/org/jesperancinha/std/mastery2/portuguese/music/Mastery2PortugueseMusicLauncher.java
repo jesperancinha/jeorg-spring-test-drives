815b9bc118bdd8d311bcf1648f3fff0c7f633e58
Joao Esperancinha <jofisaes@gmail.com>
2021-03-10 19:10:51 +0100

    🌱 jeorg-spring-mastery-2 - Pom file

---
+++ jeorg-spring-5/jeorg-spring-mastery/jeorg-spring-mastery-2/src/main/java/org/jesperancinha/std/mastery2/portuguese/music/Mastery2PortugueseMusicLauncher.java
@@ -0,0 +1,17 @@
+package org.jesperancinha.std.mastery2.portuguese.music;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mastery2PortugueseMusicLauncher implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(new Class[]{Mastery2PortugueseMusicLauncher.class}, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
