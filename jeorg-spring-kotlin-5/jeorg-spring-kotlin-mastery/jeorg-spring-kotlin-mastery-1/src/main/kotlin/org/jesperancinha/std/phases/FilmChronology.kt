5eb9cb776abcc5e76230403bf0b5fb045923876d
Joao Esperancinha <jofisaes@gmail.com>
2021-03-14 08:37:18 +0100

    🌱 jeorg-spring-kotlin-mastery-1 - Bean Initialization

--- jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/main/kotlin/org/jesperancinha/std/phases/FilmChronology.kt
+++ jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/main/kotlin/org/jesperancinha/std/phases/FilmChronology.kt
@@ -3,11 +3,17 @@ package org.jesperancinha.std.phases
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer.title
 import org.springframework.beans.BeansException
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
 
 @Component
 class FilmChronology : BeanPostProcessor {
+
+    @Value("\${management.endpoint.health.enabled}")
+    lateinit var healthEnabled: String;
+
+
     @Throws(BeansException::class)
     override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
         ConsolerizerComposer.outSpace()
@@ -23,6 +29,10 @@ class FilmChronology : BeanPostProcessor {
 
     @Throws(BeansException::class)
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        ConsolerizerComposer.outSpace()
+            .ln()
+            .autoWrite()
+            .yellow("As you can see the bean is created and it is initialized -> %s", healthEnabled)
         ConsolerizerComposer.outSpace()
             .ln()
             .autoWrite()
--- jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/main/kotlin/org/jesperancinha/std/phases/FilmChronologyFactory.kt
+++ jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/main/kotlin/org/jesperancinha/std/phases/FilmChronologyFactory.kt
@@ -1,12 +1,18 @@
 package org.jesperancinha.std.phases
 
+import org.jesperancinha.console.consolerizer.common.ConsolerizerColor
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.stereotype.Component
 
 @Component
-class FilmChronologyFactory: BeanFactoryPostProcessor {
+class FilmChronologyFactory : BeanFactoryPostProcessor {
+
+    @Value("\${management.endpoint.health.enabled}")
+    lateinit var healthEnabled: String
+
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
         ConsolerizerComposer.outSpace()
             .ln()
@@ -16,7 +22,28 @@ class FilmChronologyFactory: BeanFactoryPostProcessor {
             .bgOrange("1. postProcessBeanFactory")
             .red(ConsolerizerComposer.title("This is phase BeanFactoryPostProcessor"))
             .blue("This is bean %s", beanFactory)
-        for ( beanName in beanFactory.getBeanDefinitionNames()) {
+        val bean = beanFactory.getBean("movieLinesController")
+        ConsolerizerComposer.outSpace()
+            .ln()
+            .autoWrite()
+            .black()
+            .bgOrange("This is bean %s. If you notice it has been created", bean)
+        try {
+            ConsolerizerComposer.outSpace()
+                .ln()
+                .autoWrite()
+                .yellow("As you can see, although the beans are created, they are not initialized -> %s", healthEnabled)
+        } catch (e: UninitializedPropertyAccessException) {
+            ConsolerizerColor.RED.printExpectedException(
+                "In Kotlin is a variable is not initialized, it will result in an exception",
+                e
+            )
+            ConsolerizerComposer.outSpace()
+                .ln()
+                .autoWrite()
+                .yellow("As you can see, although the beans are created, they are not initialized")
+        }
+        for (beanName in beanFactory.getBeanDefinitionNames()) {
             ConsolerizerComposer.outSpace()
                 .ln()
                 .autoWrite()
