f18915afdf75139f6a911233c34867078ad8a87a
Joao Esperancinha <jofisaes@gmail.com>
2021-03-14 14:10:26 +0100

    🌱 jeorg-spring-action-data - CommandLineRunner

--- jeorg-spring-5/jeorg-spring-action/jeorg-spring-action-data/src/main/java/org/jesperancinha/std/action/data/JeorgActionDataLauncher.java
+++ jeorg-spring-5/jeorg-spring-action/jeorg-spring-action-data/src/main/java/org/jesperancinha/std/action/data/JeorgActionDataLauncher.java
@@ -1,11 +1,17 @@
 package org.jesperancinha.std.action.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JeorgActionDataLauncher {
+public class JeorgActionDataLauncher implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(JeorgActionDataLauncher.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
