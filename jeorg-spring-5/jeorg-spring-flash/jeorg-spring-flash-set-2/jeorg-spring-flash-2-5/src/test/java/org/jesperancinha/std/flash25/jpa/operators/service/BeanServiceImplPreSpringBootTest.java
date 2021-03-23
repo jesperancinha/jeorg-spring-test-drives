a5a66ea2267aebd37286b3adfd081dc971026804
Joao Esperancinha <jofisaes@gmail.com>
2021-03-23 08:37:50 +0100

    🌱 jeorg-spring-flash-2-5 - @MockBean

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-5/src/test/java/org/jesperancinha/std/flash25/jpa/operators/service/BeanServiceImplPreSpringBootTest.java
@@ -0,0 +1,29 @@
+package org.jesperancinha.std.flash25.jpa.operators.service;
+
+import org.jesperancinha.std.flash25.jpa.operators.repos.BeanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@MockBean(BeanRepository.class)
+@TestPropertySource("classpath:beans.properties")
+class BeanServiceImplPreSpringBootTest {
+
+    @Autowired
+    private BeanServiceImpl beanService;
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Test
+    void tesGetSlogan_whenCalled_getProductionSlogan() {
+        assertThat(beanService.getSlogan()).isEqualTo("This is just a slogan");
+        assertThat(dataSourceUrl).isEqualTo("jdbc:h2:file:~/flash25db");
+    }
+}
