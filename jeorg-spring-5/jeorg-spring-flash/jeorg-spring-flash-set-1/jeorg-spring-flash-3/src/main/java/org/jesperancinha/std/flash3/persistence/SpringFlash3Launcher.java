6a9578b734a56ea7a728790bed694d3b15e041ea
Joao Esperancinha <jofisaes@gmail.com>
2021-04-15 22:23:46 +0200

    📜 jeorg-spring-flash-3 - SpringBootTest

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-3/src/main/java/org/jesperancinha/std/flash3/persistence/SpringFlash3Launcher.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-3/src/main/java/org/jesperancinha/std/flash3/persistence/SpringFlash3Launcher.java
@@ -1,7 +1,19 @@
 package org.jesperancinha.std.flash3.persistence;
 
-public class SpringFlash3Launcher {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlash3Launcher implements ApplicationRunner {
     public static void main(String[] args) {
+        SpringApplication.run(SpringFlash3Launcher.class, args);
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
 
     }
 }
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-3/src/test/java/org/jesperancinha/std/flash3/persistence/SpringFlash3LauncherTest.java
@@ -0,0 +1,14 @@
+package org.jesperancinha.std.flash3.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlash3LauncherTest {
+
+    @Test
+    public void testContext() {
+
+    }
+
+}
