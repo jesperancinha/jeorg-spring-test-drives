d0d06a211b4939e4a2b4638d69ad85e76f53d4f7
Joao Esperancinha <jofisaes@gmail.com>
2021-04-15 22:25:52 +0200

    📜 jeorg-spring-flash-3 - SpringBootTest

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-3/src/test/java/org/jesperancinha/std/flash3/persistence/SpringFlash3LauncherTest.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-3/src/test/java/org/jesperancinha/std/flash3/persistence/SpringFlash3LauncherTest.java
@@ -1,5 +1,6 @@
 package org.jesperancinha.std.flash3.persistence;
 
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +9,20 @@ class SpringFlash3LauncherTest {
 
     @Test
     public void testContext() {
-
+        ConsolerizerComposer.outSpace()
+                .yellow("We provide the annotation this way:")
+                .blue("      <dependency>\n" +
+                        "            <groupId>org.springframework.boot</groupId>\n" +
+                        "            <artifactId>spring-boot-test</artifactId>\n" +
+                        "            <scope>test</scope>\n" +
+                        "        </dependency>")
+                .yellow("We provide the implementation this way:")
+                .blue("        <dependency>\n" +
+                        "            <groupId>org.springframework</groupId>\n" +
+                        "            <artifactId>spring-test</artifactId>\n" +
+                        "            <scope>test</scope>\n" +
+                        "        </dependency>")
+                .reset();
     }
 
 }
