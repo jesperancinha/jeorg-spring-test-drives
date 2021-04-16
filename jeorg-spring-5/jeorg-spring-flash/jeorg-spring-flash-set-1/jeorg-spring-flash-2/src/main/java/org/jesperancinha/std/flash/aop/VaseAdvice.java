47af9ac4925ecb7d35598425bef36dc6d947ef40
Joao Esperancinha <jofisaes@gmail.com>
2021-04-16 13:57:57 +0200

    🌱 jeorg-spring-flash-2 - verify

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-2/src/main/java/org/jesperancinha/std/flash/aop/VaseAdvice.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-2/src/main/java/org/jesperancinha/std/flash/aop/VaseAdvice.java
@@ -10,9 +10,9 @@ public class VaseAdvice implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
         BRIGHT_BLUE.printGenericLn("Seeds are planted!");
-        final Object proceed = methodInvocation.proceed();
-        RED.printGenericLn(proceed);
-        RED.printGenericLn(proceed.hashCode());
-        return proceed;
+        final Object object = methodInvocation.proceed();
+        RED.printGenericLn(object);
+        RED.printGenericLn(object.hashCode());
+        return object;
     }
 }
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-2/src/test/java/org/jesperancinha/std/flash/aop/VaseAdviceTest.java
@@ -0,0 +1,32 @@
+package org.jesperancinha.std.flash.aop;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class VaseAdviceTest {
+
+    @Mock
+    private MethodInvocation methodInvocation;
+
+    @Test
+    void testInvoke_whenCall_thenCallandReturnObject() throws Throwable {
+        when(methodInvocation.proceed()).thenReturn("Proceed");
+
+        final VaseAdvice vaseAdvice = new VaseAdvice();
+        final Object invoke = vaseAdvice.invoke(methodInvocation);
+
+        assertThat(invoke).isInstanceOf(String.class);
+        assertThat(invoke).isEqualTo("Proceed");
+
+        verify(methodInvocation, times(1)).proceed();
+    }
+}
