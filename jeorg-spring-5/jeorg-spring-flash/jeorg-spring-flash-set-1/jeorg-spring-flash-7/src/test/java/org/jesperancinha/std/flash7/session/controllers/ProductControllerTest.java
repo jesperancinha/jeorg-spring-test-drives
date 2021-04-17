016dd953751bfa6cb7a42afbd9fb72603909f1db
Joao Esperancinha <jofisaes@gmail.com>
2021-04-17 08:01:04 +0200

    🌱 jeorg-spring-flash-7 - 🌷🌷🌷

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-7/src/test/java/org/jesperancinha/std/flash7/session/controllers/ProductControllerTest.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-7/src/test/java/org/jesperancinha/std/flash7/session/controllers/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package org.jesperancinha.std.flash7.session.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jesperancinha.std.flash7.session.handlers.ErrorCar;
 import org.jesperancinha.std.flash7.session.handlers.ErrorFlower;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
-import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -57,17 +58,6 @@ class ProductControllerTest {
 
     @Test
     void testGetFlower_whenCalled_getResponse() throws Exception {
-        final MvcResult tulips = mockMvc.perform(get("/tulips/error"))
-                .andExpect(status().isServiceUnavailable())
-                .andReturn();
-        final var contentAsString = tulips.getResponse().getContentAsString();
-        final var errorMessage = tulips.getResponse().getErrorMessage();
-        assertThat(contentAsString).isEmpty();
-        assertThat(errorMessage).isEqualTo("We are not available at the moment!");
-    }
-
-    @Test
-    void testGetCar_whenCalled_getResponse() throws Exception {
         final ObjectMapper objectMapper = new ObjectMapper();
         final MvcResult tulips = mockMvc.perform(get("/flowers/rose"))
                 .andExpect(status().isNotFound())
@@ -81,11 +71,45 @@ class ProductControllerTest {
     }
 
     @Test
-    void testGetFlowerLocal_whenCalled_getResponse() {
+    void testGetCar_whenCalled_getResponse() throws Exception {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final MvcResult tulips = mockMvc.perform(get("/cars/kitt"))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        final var contentAsString = tulips.getResponse().getContentAsString();
+        final var errorMessage = tulips.getResponse().getErrorMessage();
+        assertThat(contentAsString).isNotNull();
+        final ErrorCar errorCar = objectMapper.readValue(contentAsString, ErrorCar.class);
+        assertThat(errorCar.getMessage()).isNotNull();
+        assertThat(errorMessage).isNull();
+    }
+
+    @Test
+    void testGetFlowerLocal_whenCalled_getResponse() throws Exception {
+        final MvcResult tulips = mockMvc.perform(get("/flowers/local/rose"))
+                .andExpect(status().isOk())
+                .andReturn();
+        final var contentAsString = tulips.getResponse().getContentAsString();
+        final var errorMessage = tulips.getResponse().getErrorMessage();
+        assertThat(contentAsString).isNotNull();
+        assertThat(contentAsString).isEmpty();
+
+        final ModelAndView modelAndView = tulips.getModelAndView();
+        assertThat(modelAndView).isNotNull();
+        final String viewName = modelAndView.getViewName();
+        assertThat(viewName).isNotNull();
+        assertThat(viewName).isEqualTo("errorpage");
+        final Map<String, Object> model = modelAndView.getModel();
+        assertThat(model).isNotNull();
+        final ErrorFlower errorFlower = ((ErrorFlower) model.get("fail"));
+        assertThat(errorFlower.getHttpStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(errorFlower.getMessage()).isNotNull();
+        assertThat(errorMessage).isNull();
     }
 
     @Test
     void testGetCarLocal_whenCalled_getResponse() {
+
     }
 
     @Test
