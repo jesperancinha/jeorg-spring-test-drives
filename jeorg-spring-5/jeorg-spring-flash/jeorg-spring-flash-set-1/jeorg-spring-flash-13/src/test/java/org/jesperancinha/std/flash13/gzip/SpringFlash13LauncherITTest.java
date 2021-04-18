83729ffd9ccdacacf40e829d15cbf01bff2bf703
Joao Esperancinha <jofisaes@gmail.com>
2021-04-18 12:59:54 +0200

    🌱 jeorg-spring-flash-13 - GZIP resource testing

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-13/src/test/java/org/jesperancinha/std/flash13/gzip/SpringFlash13LauncherITTest.java
@@ -0,0 +1,55 @@
+package org.jesperancinha.std.flash13.gzip;
+
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = SpringFlash13Launcher.class)
+class SpringFlash13LauncherITTest {
+
+    @LocalServerPort
+    private Long port;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    void testContext() {
+    }
+
+    @Test
+    void testCallsToCss_whenNormal_thenGetNormal() {
+        final var headers = new HttpHeaders();
+        final var request = new HttpEntity<>(headers);
+        var response = restTemplate.exchange(
+                String.format("http://localhost:%s/flash13.styles.css.gz", port), HttpMethod.GET,
+                request, String.class);
+        final String directZipValueString = response.getBody();
+
+        final var headers2 = new HttpHeaders();
+        headers2.set("Accept-Encoding", "gzip,deflate");
+        final var request2 = new HttpEntity<>(headers2);
+        var response2 = restTemplate.exchange(
+                String.format("http://localhost:%s/flash13.styles.css", port), HttpMethod.GET,
+                request2, String.class);
+        final String zipValueString = response2.getBody();
+
+        ConsolerizerComposer.outSpace()
+                .green(directZipValueString)
+                .reset();
+        ConsolerizerComposer.outSpace()
+                .green(zipValueString)
+                .reset();
+
+        assertThat(directZipValueString).isEqualTo(zipValueString);
+    }
+
+}
