0cc395b98a9343197e603705d6c7850af491463e
Joao Esperancinha <jofisaes@gmail.com>
2021-04-19 19:00:27 +0200

    🌱 jeorg-spring-flash-16 - 🔐 - Securing pages test

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-16/src/test/java/org/jesperancinha/std/flash16/authority/controllers/Flash16ControllerTest.java
@@ -0,0 +1,42 @@
+package org.jesperancinha.std.flash16.authority.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static java.util.Collections.singletonList;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(Flash16Controller.class)
+class Flash16ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "admin",
+            roles = "ADMIN")
+    void testGenericHandle_whenPerformingRequest_thenResponseOk() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/userdata.jsp"))
+                .andExpect(view().name("userdata"))
+                .andExpect(model().attribute("name", "admin"))
+                .andExpect(model().attribute("roles", singletonList(
+                        new SimpleGrantedAuthority("ROLE_ADMIN"))));
+    }
+
+    @Test
+    void testGenericHandle_whenPerformingRequest_thenRedirect() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(forwardedUrl(null));
+    }
+}
