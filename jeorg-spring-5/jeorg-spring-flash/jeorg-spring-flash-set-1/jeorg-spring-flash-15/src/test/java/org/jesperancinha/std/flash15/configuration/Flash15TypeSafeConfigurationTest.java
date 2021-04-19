de87d946d6cfb9ca1e3056d3bca9921cd562d880
Joao Esperancinha <jofisaes@gmail.com>
2021-04-19 08:01:46 +0200

    🌱 jeorg-spring-flash-15 - 🏎 Configuration test

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-15/src/test/java/org/jesperancinha/std/flash15/configuration/Flash15TypeSafeConfigurationTest.java
@@ -0,0 +1,22 @@
+package org.jesperancinha.std.flash15.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class Flash15TypeSafeConfigurationTest {
+
+    @Autowired
+    private Flash15TypeSafeConfiguration flash15TypeSafeConfiguration;
+
+    @Test
+    public void testConfiguration_whenTypeSafe_thenCorrectConfiguration() {
+        assertThat(flash15TypeSafeConfiguration.getLyric3()).isEqualTo("Don't wanna lie here");
+        assertThat(flash15TypeSafeConfiguration.getLyric4()).isEqualTo("But you can learn to");
+    }
+
+}
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-15/src/test/java/org/jesperancinha/std/flash15/configuration/SpringFlash15LauncherRestTest.java
@@ -0,0 +1,55 @@
+package org.jesperancinha.std.flash15.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class SpringFlash15LauncherRestTest {
+
+    @MockBean
+    private Flash15TypeSafeConfiguration flash15TypeSafeConfiguration;
+
+    @MockBean
+    private Flash15TraditionalConfiguration flash15TraditionalConfiguration;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetResponse_whenCalling_thenReturnConfiguration() throws Exception {
+        when(flash15TraditionalConfiguration.getLyric1()).thenReturn("got");
+        when(flash15TraditionalConfiguration.getLyric2()).thenReturn("everything");
+        when(flash15TypeSafeConfiguration.getLyric3()).thenReturn("I");
+        when(flash15TypeSafeConfiguration.getLyric4()).thenReturn("wanted");
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        "\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84" +
+                                "<p style=\"margin:0; font-family: monospace; color:#0000aa\"><b>got</b></p>" +
+                                "<p style=\"margin:0; font-family: monospace; color:#0000aa\"><b>everything</b></p>" +
+                                "<p style=\"margin:0; font-family: monospace; color:#0000aa\"><b>I</b></p>" +
+                                "<p style=\"margin:0; font-family: monospace; color:#0000aa\"><b>wanted</b></p>" +
+                                "\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84\uD83E\uDD84"));
+
+        verify(flash15TraditionalConfiguration, times(0)).setLyric1(any());
+        verify(flash15TraditionalConfiguration, times(1)).getLyric1();
+        verify(flash15TraditionalConfiguration, times(0)).setLyric2(any());
+        verify(flash15TraditionalConfiguration, times(1)).getLyric2();
+        verify(flash15TypeSafeConfiguration, times(0)).setLyric3(any());
+        verify(flash15TypeSafeConfiguration, times(1)).getLyric3();
+        verify(flash15TypeSafeConfiguration, times(0)).setLyric4(any());
+        verify(flash15TypeSafeConfiguration, times(1)).getLyric4();
+    }
+}
