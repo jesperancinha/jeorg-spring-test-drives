18e5297f3e010143666d391bcf9f93e3f56d12e3
Joao Esperancinha <jofisaes@gmail.com>
2021-04-20 23:34:44 +0200

    🌱 jeorg-spring-flash-19 - 📚 - testCreateAlbumRollBack_whenCall_thenSimulatePerformARollback

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-19/src/test/java/org/jesperancinha/std/flash19/transactional/controllers/AlbumControllerTest.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-19/src/test/java/org/jesperancinha/std/flash19/transactional/controllers/AlbumControllerTest.java
@@ -5,15 +5,24 @@ import org.jesperancinha.std.flash19.transactional.domain.AlbumRepository;
 import org.jesperancinha.std.flash19.transactional.services.AlbumServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -21,12 +30,14 @@ import static org.mockito.Mockito.when;
 @ContextConfiguration(classes = AlbumServiceImpl.class)
 class AlbumControllerTest {
 
-
-    @Autowired
+    @SpyBean(AlbumRepository.class)
     private AlbumRepository albumRepository;
 
     @Autowired
     private AlbumServiceImpl albumService;
+
+    @Captor
+    private ArgumentCaptor<Album> albumArgumentCaptor;
 
     @Test
     void testGetAllAlbums_whenCall_thenGetAllAlbums() {
@@ -39,7 +50,18 @@ class AlbumControllerTest {
     }
 
     @Test
-    void createAlbumRollBack() {
+    void testCreateAlbumRollBack_whenCall_thenSimulatePerformARollback() {
+        assertThrows(RuntimeException.class,()->
+                albumService.createAlbumRollBack(
+                        "The Downward Spiral", "Nine Inch Nails", "Nothing Records", 1995L));
+
+        verify(albumRepository, times(1)).save(albumArgumentCaptor.capture());
+        final Album value = albumArgumentCaptor.getValue();
+        assertThat(value.getId()).isNull();
+        assertThat(value.getName()).isEqualTo("The Downward Spiral");
+        assertThat(value.getArtist()).isEqualTo("Nine Inch Nails");
+        assertThat(value.getPublisher()).isEqualTo("Nothing Records");
+        assertThat(value.getYear()).isEqualTo(1995L);
     }
 
     @Test
