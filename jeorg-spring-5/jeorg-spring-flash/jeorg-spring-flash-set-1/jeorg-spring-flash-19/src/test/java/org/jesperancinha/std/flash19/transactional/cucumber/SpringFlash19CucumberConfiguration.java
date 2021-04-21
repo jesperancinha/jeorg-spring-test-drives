bcded6b44986b9ddb0c3fb3b9743e0de1781777d
Joao Esperancinha <jofisaes@gmail.com>
2021-04-21 14:52:29 +0200

    🌱 jeorg-spring-flash-19 - 🥒 - Typo

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-19/src/test/java/org/jesperancinha/std/flash19/transactional/cucumber/SpringFlash19CucumberConfiguration.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-19/src/test/java/org/jesperancinha/std/flash19/transactional/cucumber/SpringFlash19CucumberConfiguration.java
@@ -3,7 +3,10 @@ package org.jesperancinha.std.flash19.transactional.cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Posted on twitter as SprinfFlash19CucumberConfiguration, this is the configuration which enables Spring IoC beans to be injected accross the Cucumber Steps.
+ */
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SprinfFlash19CucumberConfiguration {
+public class SpringFlash19CucumberConfiguration {
 }
