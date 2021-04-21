44015efe00624e4cb898cd655711f0ff1c14b805
Joao Esperancinha <jofisaes@gmail.com>
2021-04-21 17:43:25 +0200

    🌱 jeorg-spring-flash-19 - 🥒 - Typo

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-20/src/main/java/org/jesperancinha/std/flash20/cors/Flash20Controller.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-20/src/main/java/org/jesperancinha/std/flash20/cors/Flash20Controller.java
@@ -10,7 +10,7 @@ public class Flash20Controller {
     @GetMapping("/cors")
     @CrossOrigin(origins = "http://jeorg-spring-flash-20:8081")
     public String sentence() {
-        return "When accessing from jeorg-spring-flash-20, there should be no blocking to visualizatin";
+        return "When accessing from jeorg-spring-flash-20, there should be no blocking to visualization";
     }
 
     @GetMapping("/always")
@@ -20,7 +20,7 @@ public class Flash20Controller {
 
     @GetMapping("/protected")
     public String sentenceProtected() {
-        return "When accessing from jeorg-spring-flash-20, there should be no blocking to visualizatin";
+        return "When accessing from jeorg-spring-flash-20, there should be no blocking to visualization";
     }
 
 
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-20/src/test/java/org/jesperancinha/std/flash20/cors/Flash20ControllerTest.java
@@ -0,0 +1,70 @@
+package org.jesperancinha.std.flash20.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(Flash20Controller.class)
+@ImportResource("classpath:WEB-INF/beans.xml")
+class Flash20ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSentence_whenCalled_thenShowSentence() throws Exception {
+        mockMvc.perform(get("/cors"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("When accessing from jeorg-spring-flash-20, there should be no blocking to visualization"));
+    }
+
+    @Test
+    void testSentence_whenCalledWithDifferentOrigin_thenFail() throws Exception {
+        mockMvc.perform(get("/cors")
+                .header("Origin", "http://thissiteissomethingcopletelydifferentlocalhost.com"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Invalid CORS request"));
+    }
+
+    @Test
+    void testSentenceAlways_whenCalled_thenShowSentence() throws Exception {
+        mockMvc.perform(get("/always"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("This should be blocked from the opposite url."));
+    }
+
+    @Test
+    void testSentenceAlways_whenCalledDifferentOrigin_thenShowSentence() throws Exception {
+        mockMvc.perform(get("/always")
+                .header("Origin", "http://thissiteissomethingcopletelydifferentlocalhost.com"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("This should be blocked from the opposite url."));
+    }
+
+    @Test
+    void testSentenceProtected_whenCalled_thenShowSentence() throws Exception {
+        mockMvc.perform(get("/protected"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("When accessing from jeorg-spring-flash-20, there should be no blocking to visualization"));
+    }
+
+    @Test
+    void testSentenceProtected_whenCalledDiffentOrigint_thenFail() throws Exception {
+        mockMvc.perform(get("/protected")
+                .header("Origin", "http://thissiteissomethingcopletelydifferentlocalhost.com"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Invalid CORS request"));
+    }
+}
