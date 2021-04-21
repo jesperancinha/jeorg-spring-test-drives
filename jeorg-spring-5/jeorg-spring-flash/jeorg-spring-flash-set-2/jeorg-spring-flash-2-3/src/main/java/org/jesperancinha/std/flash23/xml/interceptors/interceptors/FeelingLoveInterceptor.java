5791d4468d6a236078c4548e5f8fb20a201b7fda
Joao Esperancinha <jofisaes@gmail.com>
2021-04-21 23:00:48 +0200

    🌱 jeorg-spring-flash-2-3 - ♥️ - Feeling love interceptor unit tests with beans.xml

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-3/src/main/java/org/jesperancinha/std/flash23/xml/interceptors/interceptors/FeelingLoveInterceptor.java
@@ -0,0 +1,37 @@
+package org.jesperancinha.std.flash23.xml.interceptors.interceptors;
+
+import org.jesperancinha.std.flash23.xml.interceptors.beans.FeelingLoveBean;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FeelingLoveInterceptor implements HandlerInterceptor {
+
+    private final FeelingLoveBean feelingLoveBean;
+
+    public FeelingLoveInterceptor(FeelingLoveBean feelingLoveBean) {
+        this.feelingLoveBean = feelingLoveBean;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) {
+
+        return feelingLoveBean.preHandle(handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) {
+        feelingLoveBean.postHandle(handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception exception) {
+        feelingLoveBean.afterCompletion(handler, exception);
+    }
+}
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-3/src/test/java/org/jesperancinha/std/flash23/xml/interceptors/SpringFlash23LauncherTest.java
@@ -0,0 +1,71 @@
+package org.jesperancinha.std.flash23.xml.interceptors;
+
+import org.jesperancinha.std.flash23.xml.interceptors.beans.FeelingLoveBean;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SpringFlash23Launcher.class)
+@ImportResource("classpath:WEB-INF/beans.xml")
+class SpringFlash23LauncherTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FeelingLoveBean feelingLoveBean;
+
+    @Captor
+    private ArgumentCaptor<Object> objectArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<ModelAndView> modelAndViewArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<Exception> exceptionArgumentCaptor;
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void testGetString_whenCalled_thenTriggerInterceptors() throws Exception {
+        when(feelingLoveBean.preHandle(any(Object.class))).thenReturn(true);
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Fine Wine"));
+
+        verify(feelingLoveBean, times(1)).preHandle(objectArgumentCaptor.capture());
+        verify(feelingLoveBean, times(1)).postHandle(objectArgumentCaptor.capture(), modelAndViewArgumentCaptor.capture());
+        verify(feelingLoveBean, times(1)).afterCompletion(objectArgumentCaptor.capture(), exceptionArgumentCaptor.capture());
+
+        final List<Object> allValues = objectArgumentCaptor.getAllValues();
+        assertThat(allValues).hasSize(3);
+        allValues.forEach(value ->
+                assertThat(value.toString()).isEqualTo("org.jesperancinha.std.flash23.xml.interceptors.SpringFlash23Launcher#getString()"));
+        final ModelAndView value = modelAndViewArgumentCaptor.getValue();
+        assertThat(value).isNull();
+        final Exception exception = exceptionArgumentCaptor.getValue();
+        assertThat(exception).isNull();
+
+    }
+}
