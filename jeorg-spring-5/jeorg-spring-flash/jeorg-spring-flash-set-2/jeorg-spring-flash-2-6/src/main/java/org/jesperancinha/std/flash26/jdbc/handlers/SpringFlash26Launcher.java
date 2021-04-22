bc496ddf10657c9f8131c22e8e4eafde8c2d4a2a
Joao Esperancinha <jofisaes@gmail.com>
2021-04-22 19:10:39 +0200

    🌱 jeorg-spring-flash-2-6 - 🐚 - RowMapper vs ResultSetExtractor

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-6/src/main/java/org/jesperancinha/std/flash26/jdbc/handlers/SpringFlash26Launcher.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-6/src/main/java/org/jesperancinha/std/flash26/jdbc/handlers/SpringFlash26Launcher.java
@@ -1,6 +1,6 @@
 package org.jesperancinha.std.flash26.jdbc.handlers;
 
-import org.jesperancinha.console.consolerizer.console.Consolerizer;
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer;
 import org.jesperancinha.std.flash26.jdbc.handlers.model.Shell;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,11 +17,12 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.jesperancinha.console.consolerizer.console.Consolerizer.printRainbowTitleLn;
 import static org.jesperancinha.console.consolerizer.common.ConsolerizerColor.BLUE;
 import static org.jesperancinha.console.consolerizer.common.ConsolerizerColor.GREEN;
 import static org.jesperancinha.console.consolerizer.common.ConsolerizerColor.MAGENTA;
 import static org.jesperancinha.console.consolerizer.common.ConsolerizerColor.RED;
+import static org.jesperancinha.console.consolerizer.console.Consolerizer.printRainbowTitleLn;
+import static org.jesperancinha.console.consolerizer.console.ConsolerizerComposer.title;
 
 @SpringBootApplication
 public class SpringFlash26Launcher implements CommandLineRunner {
@@ -37,59 +38,80 @@ public class SpringFlash26Launcher implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Consolerizer.setupFastDefault();
+    public void run(String... args) {
+        testNoResultQueryOnResultSetExtractor();
+        initializeDatabase();
+        final List<Object[]> data = Arrays.asList(
+                new Object[]{"Berbigão", "Cerastoderma edule", "white"},
+                new Object[]{"Conquilha", " Donax trunculus", "blue"}
+        );
+        insertData(data);
+        final String query = testResultSetExtractor();
+        ConsolerizerComposer.outSpace()
+                .cyan(title("Testing ResultSetExtractor"))
+                .magenta(query)
+                .reset();
+        final List<Shell> allShells = testRowMapper();
+        ConsolerizerComposer.outSpace()
+                .cyan(title("Testing RowMapper"))
+                .magenta(allShells)
+                .reset();
+        ConsolerizerComposer.outSpace()
+                .blue("All Shells -> %s", allShells)
+                .blue("The result is %s", query)
+                .magenta("Just like RowMapper, a ResultSetExtractor gives us the possibility to get all results")
+                .magenta("The Difference is that while RowMapper allows us to map a row to its index number from the result set, a ResultSetExtractor just returns the result set that we then have to iterate.")
+                .reset();
+    }
+
+    private List<Shell> testRowMapper() {
+        return jdbcTemplate.query("select name, scientificName, predominentColor from shells"
+                , (rs, rowNum) -> {
+                    GREEN.printGenericLn("The shell best known as %s, has a scientific name of %s and is mostly found with color %s",
+                            rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
+
+                    return new Shell(rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
+                });
+    }
+
+    private String testResultSetExtractor() {
+        return jdbcTemplate.query("select name, scientificName, predominentColor from shells"
+                , rs -> {
+                    while (rs.next()) {
+                        printRainbowTitleLn("The shell best known as %s, has a scientific name of %s and is mostly found with color %s",
+                                rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
+                    }
+                    return "OK!";
+                });
+    }
+
+    private void insertData(List<Object[]> data) {
+        jdbcTemplate.batchUpdate("insert into shells(name, scientificName, predominentColor) values (?,?,?)",
+                data);
+    }
+
+    private void initializeDatabase() {
+        jdbcTemplate.execute("drop table shells if exists");
+        jdbcTemplate.execute("create table shells(id serial, name varchar(255), scientificName varchar(255), predominentColor varchar(255))");
+    }
+
+    private void testNoResultQueryOnResultSetExtractor() {
         try {
             jdbcTemplate.query("create table shells(id serial, name varchar(255), scientificName varchar(255), predominentColor varchar(255))"
                     , new ResultSetExtractor<String>() {
                         @Override
                         public String extractData(ResultSet rs) throws SQLException, DataAccessException {
+                            ConsolerizerComposer.outSpace()
+                                    .red("A create query will not yield any results")
+                                    .red("This means that if the code enters this scope,")
+                                    .red("we should end the program, since this isn't necessary")
+                                    .reset();
+                            System.exit(1);
                             return null;
                         }
                     });
         } catch (final UncategorizedSQLException uncategorizedSQLException) {
             RED.printExpectedException("This is an expected exception, because only queries are allowed. Although a create is sometimes referred to as a query, in this context, it must return results.", uncategorizedSQLException);
         }
-
-        jdbcTemplate.execute("drop table shells if exists");
-        jdbcTemplate.execute("create table shells(id serial, name varchar(255), scientificName varchar(255), predominentColor varchar(255))");
-
-        jdbcTemplate.batchUpdate("insert into shells(name, scientificName, predominentColor) values (?,?,?)",
-                Arrays.asList(
-                        new Object[]{"Berbigão", "Cerastoderma edule", "white"},
-                        new Object[]{"Conquilha", " Donax trunculus", "blue"}
-                ));
-
-
-        final String query = jdbcTemplate.query("select name, scientificName, predominentColor from shells"
-                , new ResultSetExtractor<String>() {
-                    @Override
-                    public String extractData(
-                            @NonNull
-                                    ResultSet rs) throws SQLException, DataAccessException {
-                        while (rs.next()) {
-                            printRainbowTitleLn("The shell best known as %s, has a scientific name of %s and is mostly found with color %s",
-                                    rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
-                        }
-                        return "OK!";
-                    }
-                });
-
-
-        final List<Shell> allShells = jdbcTemplate.query("select name, scientificName, predominentColor from shells"
-                , new RowMapper<Shell>() {
-                    @Override
-                    public Shell mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        GREEN.printGenericLn("The shell best known as %s, has a scientific name of %s and is mostly found with color %s",
-                                rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
-
-                        return new Shell(rs.getString("name"), rs.getString("scientificName"), rs.getString("predominentColor"));
-                    }
-                });
-
-        BLUE.printGenericLn("All Shells -> %s", allShells);
-        BLUE.printGenericTitleLn("The result is %s", query);
-        MAGENTA.printGenericLn("Just like RowMapper, a ResultSetExtractor gives us the possibility to get all results");
-        MAGENTA.printGenericLn("The Difference is that while RowMapper allows us to map a row to its index number from the result set, a ResultSetExtractor just returns the result set that we then have to iterate.");
     }
 }
