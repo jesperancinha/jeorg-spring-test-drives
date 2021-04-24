11bd9c9a3e4ee9ac12a768d52ee4e1dfd2ca0c50
Joao Esperancinha <jofisaes@gmail.com>
2021-04-24 12:26:46 +0200

    🌱 jeorg-spring-flash-2-10 - 🌎 - spring.factories

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-10/src/test/java/org/jesperancinha/std/flash210/factory/SpringFlash210LauncherTest.java
@@ -0,0 +1,27 @@
+package org.jesperancinha.std.flash210.factory;
+
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer;
+import org.jesperancinha.std.flash210.factory.configurations.Material;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SpringFlash210LauncherTest {
+
+    @Autowired
+    private Material material;
+
+    @Test
+    void testContext() {
+        assertThat(material).isNotNull();
+        final var value = material.getValue();
+        assertThat(value).isEqualTo("The world is not enough!");
+        ConsolerizerComposer.outSpace()
+                .brightBlue()
+                .jsonPrettyPrint(material)
+                .reset();
+    }
+}
