241612b7a434c77ac0342e0006a651d75650ffd0
Joao Esperancinha <jofisaes@gmail.com>
2021-04-26 20:06:11 +0200

    🌱 jeorg-spring-flash-2-19 - 🥜 - bean methods

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-19/src/test/java/org/jesperancinha/std/flash219/beans/SpringFlash219LauncherTest.java
@@ -0,0 +1,14 @@
+package org.jesperancinha.std.flash219.beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringFlash219LauncherTest {
+
+    @Test
+    void testContext() {
+    }
+}
