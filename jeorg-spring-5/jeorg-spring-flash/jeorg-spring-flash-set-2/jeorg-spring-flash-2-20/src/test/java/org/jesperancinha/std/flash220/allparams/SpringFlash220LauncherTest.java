92a67478193bbb72b123d4c858cb2a40a4f52f69
Joao Esperancinha <jofisaes@gmail.com>
2021-04-27 09:25:24 +0200

    🌱 jeorg-spring-flash-2-20 - 7️⃣ - Testing Matrix request params

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-20/src/test/java/org/jesperancinha/std/flash220/allparams/SpringFlash220LauncherTest.java
@@ -0,0 +1,11 @@
+package org.jesperancinha.std.flash220.allparams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlash220LauncherTest {
+    @Test
+    void main() {
+    }
+}
