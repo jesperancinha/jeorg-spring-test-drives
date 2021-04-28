04a3c5d256cda68620f21ca4a4eaf4708f2baaca
Joao Esperancinha <jofisaes@gmail.com>
2021-04-28 09:14:04 +0200

    🌱 jeorg-spring-flash-3-3 - 📺 - Integration Tests 2

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-3/jeorg-spring-flash-3-3/src/test/java/org/jesperancinha/std/flash33/rollback/transactional/services/it/EpisodeServiceIT.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-3/jeorg-spring-flash-3-3/src/test/java/org/jesperancinha/std/flash33/rollback/transactional/services/it/EpisodeServiceIT.java
@@ -1,5 +1,6 @@
 package org.jesperancinha.std.flash33.rollback.transactional.services.it;
 
+import org.jesperancinha.std.flash33.rollback.transactional.domain.Episode;
 import org.jesperancinha.std.flash33.rollback.transactional.dto.EpisodeDto;
 import org.jesperancinha.std.flash33.rollback.transactional.exceptions.EpisodeException;
 import org.jesperancinha.std.flash33.rollback.transactional.exceptions.VideoCountryException;
@@ -9,8 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -25,12 +24,12 @@ class EpisodeServiceIT {
     private EpisodeService episodeService;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         episodeRepository.deleteAll();
     }
 
     @Test
-     void testCreateEpisode_whenCreateOne_thenCallSave() {
+    void testCreateEpisode_whenCreateOne_thenCallSave() {
         final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
 
         assertThatExceptionOfType(EpisodeException.class)
@@ -59,59 +58,80 @@ class EpisodeServiceIT {
         assertThat(all).isNotNull();
         assertThat(all).hasSize(0);
     }
-//
-//    @Test
-//    void testCreateEpisodeExceptionNoRollback_whenCreateOne_thenCallSave() {
-//        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
-//
-//        assertThatExceptionOfType(EpisodeException.class)
-//                .isThrownBy(() -> episodeService.createEpisodeExceptionNoRollback(episodeDto));
-//
-//        verify(episodeRepository, times(1))
-//                .save(Episode.builder().id(1L).name(episodeDto.getName()).build());
-//    }
-//
-//    @Test
-//    void testCreateEpisodeMixRollback_whenCreateOne_thenCallSave() {
-//        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
-//
-//        assertThatExceptionOfType(VideoCountryException.class)
-//                .isThrownBy(() -> episodeService.createEpisodeMixRollback(episodeDto));
-//
-//        verify(episodeRepository, times(1))
-//                .save(Episode.builder().id(1L).name(episodeDto.getName()).build());
-//    }
-//
-//    @Test
-//    void testCreateEpisodeMixNoRollback_whenCreateOne_thenCallSave() {
-//        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
-//
-//        assertThatExceptionOfType(EpisodeException.class)
-//                .isThrownBy(() -> episodeService.createEpisodeMixNoRollback(episodeDto));
-//
-//        verify(episodeRepository, times(1))
-//                .save(Episode.builder().id(1L).name(episodeDto.getName()).build());
-//    }
-//
-//    @Test
-//    void testGetEpisodeById_whenGetById1_thenGetEpisode1() {
-//        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
-//        when(episodeRepository.findById(1L)).thenReturn(Optional.of(Episode.builder().id(1L).name(episodeDto.getName()).build()));
-//
-//        final var resultEpisodeDto = episodeService.getEpisodeById(1L);
-//
-//        assertThat(resultEpisodeDto).isEqualTo(episodeDto);
-//
-//    }
-//
-//    @Test
-//    void getAllEpisodes() {
-//        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
-//        final var episodes = List.of(Episode.builder().id(1L).name(episodeDto.getName()).build());
-//        when(episodeRepository.findAll()).thenReturn(episodes);
-//
-//        final var resultEpisodeDtos = episodeService.getAllEpisodes();
-//
-//        assertThat(resultEpisodeDtos).isEqualTo(List.of(episodeDto));
-//    }
+
+    @Test
+    void testCreateEpisodeExceptionNoRollback_whenCreateOne_thenCallSave() {
+        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
+
+        assertThatExceptionOfType(EpisodeException.class)
+                .isThrownBy(() -> episodeService.createEpisodeExceptionNoRollback(episodeDto));
+
+        final var all = episodeRepository.findAll();
+
+        assertThat(all).isNotNull();
+        assertThat(all).hasSize(1);
+        final var episode = all.get(0);
+        assertThat(episode).isNotNull();
+        assertThat(episode.getId()).isNotNull();
+        assertThat(episode.getId()).isBetween(1L, 10L);
+        assertThat(episode.getName()).isEqualTo(episodeDto.getName());
+    }
+
+    @Test
+    void testCreateEpisodeMixRollback_whenCreateOne_thenCallSave() {
+        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
+
+        assertThatExceptionOfType(VideoCountryException.class)
+                .isThrownBy(() -> episodeService.createEpisodeMixRollback(episodeDto));
+
+        final var all = episodeRepository.findAll();
+
+        assertThat(all).isNotNull();
+        assertThat(all).hasSize(0);
+    }
+
+    @Test
+    void testCreateEpisodeMixNoRollback_whenCreateOne_thenCallSave() {
+        final var episodeDto = EpisodeDto.builder().id(1L).name("The eyes see more").build();
+
+        assertThatExceptionOfType(EpisodeException.class)
+                .isThrownBy(() -> episodeService.createEpisodeMixNoRollback(episodeDto));
+
+        final var all = episodeRepository.findAll();
+
+        assertThat(all).isNotNull();
+        assertThat(all).hasSize(1);
+        final var episode = all.get(0);
+        assertThat(episode).isNotNull();
+        assertThat(episode.getId()).isNotNull();
+        assertThat(episode.getId()).isBetween(1L, 10L);
+        assertThat(episode.getName()).isEqualTo(episodeDto.getName());
+    }
+
+    @Test
+    void testGetEpisodeById_whenGetById1_thenGetEpisode1() {
+        final var episode = Episode.builder().id(1L).name("The eyes see more").build();
+        final var savedEpisode = episodeRepository.save(episode);
+
+        final var resultEpisodeDto = episodeService.getEpisodeById(savedEpisode.getId());
+
+        assertThat(resultEpisodeDto).isNotNull();
+        assertThat(resultEpisodeDto.getId()).isEqualTo(savedEpisode.getId());
+        assertThat(resultEpisodeDto.getName()).isEqualTo(savedEpisode.getName());
+    }
+
+    @Test
+    void testGetAllEpisodes_whenGetAll_thenListAllEpisodes() {
+        final var episode = Episode.builder().id(1L).name("The eyes see more").build();
+        final var savedEpisode = episodeRepository.save(episode);
+
+        final var resultEpisodeDtos = episodeService.getAllEpisodes();
+
+        assertThat(resultEpisodeDtos).isNotNull();
+        assertThat(resultEpisodeDtos).hasSize(1);
+        final var episodeDto = resultEpisodeDtos.get(0);
+        assertThat(episodeDto).isNotNull();
+        assertThat(episodeDto.getId()).isEqualTo(savedEpisode.getId());
+        assertThat(episodeDto.getName()).isEqualTo(savedEpisode.getName());
+    }
 }
