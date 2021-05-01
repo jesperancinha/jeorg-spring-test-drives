960e6ec0211d338c892c96e0b4858b4da876d914
Joao Esperancinha <jofisaes@gmail.com>
2021-05-01 10:23:51 +0200

    🌱 jeorg-spring-flash-3-14 - 🍲 - Proxy Soup preparations

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-3/jeorg-spring-flash-3-14/src/test/java/org/jesperancinha/std/flash314/jdk/enhancer/EnhancerCrum4Test.java
@@ -0,0 +1,11 @@
+package org.jesperancinha.std.flash314.jdk.enhancer;
+
+import org.junit.jupiter.api.Test;
+
+class EnhancerCrum4Test {
+
+    @Test
+    void testContext() {
+        EnhancerCrum4.main(null);
+    }
+}
