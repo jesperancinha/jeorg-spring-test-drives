7c8a5c6a8c7d2ce4c4dc9f257cd51a2869595625
Joao Esperancinha <jofisaes@gmail.com>
2021-05-02 18:05:00 +0200

    🌱 jeorg-spring-flash-4-2 - 📂 - Testing Context

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-4/jeorg-spring-flash-4-2/src/test/java/org/jesperancinha/std/flash42/jsp/security/profiles/SpringFlash42LauncherTest.java
@@ -0,0 +1,12 @@
+package org.jesperancinha.std.flash42.jsp.security.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlash42LauncherTest {
+
+    @Test
+    void testContext() {
+    }
+}
