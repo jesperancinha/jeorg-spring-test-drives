e2983b0737d501459f87667d4ff47fd4d487c64c
Joao Esperancinha <jofisaes@gmail.com>
2021-05-04 12:54:46 +0200

    🌱 jeorg-spring-flash-5-5 - 🪢 - HATEOS

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-5/jeorg-spring-flash-5-5/src/main/java/org/jesperancinha/std/flash55/hateoas/SpringFlash55Launcher.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-5/jeorg-spring-flash-5-5/src/main/java/org/jesperancinha/std/flash55/hateoas/SpringFlash55Launcher.java
@@ -19,7 +19,7 @@ public class SpringFlash55Launcher implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
     }
 
---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-5/jeorg-spring-flash-5-5/src/test/java/org/jesperancinha/std/flash55/hateoas/SpringFlash55LauncherTest.java
@@ -0,0 +1,26 @@
+package org.jesperancinha.std.flash55.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringFlash55LauncherTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void getAllCells() {
+    }
+
+    @Test
+    void getAllCellsEndless() {
+    }
+}
