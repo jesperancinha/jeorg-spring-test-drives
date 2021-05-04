38c989fbe9bb76891f631920f72c1603f41a5f74
Joao Esperancinha <jofisaes@gmail.com>
2021-05-04 13:16:55 +0200

    🌱 jeorg-spring-flash-5-7 - 📖 CRUD operations with the JdbcTemplate

---
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-5/jeorg-spring-flash-5-6/src/test/java/org/jesperancinha/std/flash56/sql/standards/SpringFlash56LauncherTest.java
@@ -0,0 +1,12 @@
+package org.jesperancinha.std.flash56.sql.standards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlash56LauncherTest {
+
+    @Test
+    void testContext() {
+    }
+}
