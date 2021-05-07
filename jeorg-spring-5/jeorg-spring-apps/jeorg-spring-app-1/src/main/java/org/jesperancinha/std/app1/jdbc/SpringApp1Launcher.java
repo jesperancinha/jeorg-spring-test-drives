edae104b8878b8e15e7a39288c21d8937c476690
Joao Esperancinha <jofisaes@gmail.com>
2021-05-07 21:38:29 +0200

    🌱 jeorg-spring-action-aop - 🏗 - More tests

--- jeorg-spring-5/jeorg-spring-apps/jeorg-spring-app-1/src/main/java/org/jesperancinha/std/app1/jdbc/SpringApp1Launcher.java
+++ jeorg-spring-5/jeorg-spring-apps/jeorg-spring-app-1/src/main/java/org/jesperancinha/std/app1/jdbc/SpringApp1Launcher.java
@@ -1,10 +1,11 @@
 package org.jesperancinha.std.app1.jdbc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApp1Launcher {
     public static void main(String[] args) {
-
+        SpringApplication.run(SpringApp1Launcher.class, args);
     }
 }
