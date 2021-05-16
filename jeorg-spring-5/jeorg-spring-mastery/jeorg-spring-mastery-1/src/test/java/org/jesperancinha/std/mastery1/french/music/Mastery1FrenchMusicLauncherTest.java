edfdc25c133444286e47dbabe8ec00727888cd66
Joao Esperancinha <jofisaes@gmail.com>
2021-05-16 19:42:51 +0200

    🌱 jeorg-spring-mastery-1 - Coverage

---
+++ jeorg-spring-5/jeorg-spring-mastery/jeorg-spring-mastery-1/src/test/java/org/jesperancinha/std/mastery1/french/music/Mastery1FrenchMusicLauncherTest.java
@@ -0,0 +1,17 @@
+package org.jesperancinha.std.mastery1.french.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class Mastery1FrenchMusicLauncherTest {
+
+    @Test
+    void testContext() {
+        Mastery1FrenchMusicLauncher.main(new String[0]);
+    }
+}
