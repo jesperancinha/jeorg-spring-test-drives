46d0c757e4a0c1df03d28ad7e2361c7e087dbd5b
Joao Esperancinha <jofisaes@gmail.com>
2021-05-16 20:19:45 +0200

    🌱 jeorg-spring-action-mvc - 📡 - A view resolver for MVC

---
+++ jeorg-spring-5/jeorg-spring-action/jeorg-spring-action-mvc/src/main/java/org/jesperancinha/std/action/mvc/configuration/ElectronicaConfiguration.java
@@ -0,0 +1,18 @@
+package org.jesperancinha.std.action.mvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ElectronicaConfiguration {
+
+    @Bean
+    public ViewResolver jspViewResolver() {
+        final var internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
---
+++ jeorg-spring-5/jeorg-spring-action/jeorg-spring-action-mvc/src/test/java/org/jesperancinha/std/action/mvc/JeorgActionMvcLauncherTest.java
@@ -0,0 +1,14 @@
+package org.jesperancinha.std.action.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JeorgActionMvcLauncherTest {
+
+    @Test
+    void testContext() {
+    }
+}
