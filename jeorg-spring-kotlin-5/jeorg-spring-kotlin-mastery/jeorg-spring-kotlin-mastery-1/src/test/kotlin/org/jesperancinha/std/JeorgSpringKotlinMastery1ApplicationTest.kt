aba7e729a7f3764545f5ede07b950658ae154919
Joao Esperancinha <jofisaes@gmail.com>
2021-11-18 20:09:20 +0100

    🌱 Dirties Context Handy Annotations

--- jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/test/kotlin/org/jesperancinha/std/JeorgSpringKotlinMastery1ApplicationTest.kt
+++ jeorg-spring-kotlin-5/jeorg-spring-kotlin-mastery/jeorg-spring-kotlin-mastery-1/src/test/kotlin/org/jesperancinha/std/JeorgSpringKotlinMastery1ApplicationTest.kt
@@ -2,8 +2,15 @@ package org.jesperancinha.std
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD
+import org.springframework.test.annotation.DirtiesContext.HierarchyMode.EXHAUSTIVE
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener
 
 @SpringBootTest("test=123")
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD, hierarchyMode = EXHAUSTIVE)
+@TestExecutionListeners(DirtiesContextTestExecutionListener::class)
 class JeorgSpringKotlinMastery1ApplicationTest {
 
     @Test
