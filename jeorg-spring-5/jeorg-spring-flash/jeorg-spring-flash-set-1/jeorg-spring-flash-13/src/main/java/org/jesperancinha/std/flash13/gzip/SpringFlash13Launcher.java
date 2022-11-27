3e6f02ddd10691411696db87bd4fc6884b9752c0
Joao Esperancinha <jofisaes@gmail.com>
2022-11-27 18:10:31 +0100

    Removes GzipResourceResolver

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-13/src/main/java/org/jesperancinha/std/flash13/gzip/SpringFlash13Launcher.java
+++ jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-13/src/main/java/org/jesperancinha/std/flash13/gzip/SpringFlash13Launcher.java
@@ -7,7 +7,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.EncodedResourceResolver;
-import org.springframework.web.servlet.resource.GzipResourceResolver;
 
 @SpringBootApplication
 public class SpringFlash13Launcher {
@@ -15,24 +14,17 @@ public class SpringFlash13Launcher {
         SpringApplication.run(SpringFlash13Launcher.class, args);
     }
 
-    /**
-     * I leave both {@link EncodedResourceResolver} and {@link GzipResourceResolver} in the chain because both work
-     * {@link EncodedResourceResolver} is the new version and {@link GzipResourceResolver} is now deprecated.
-     *
-     * @return {@link WebMvcConfigurer}
-     */
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addResourceHandlers(
                     @NonNull
-                            ResourceHandlerRegistry registry) {
+                    ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/static/**")
                         .addResourceLocations("classpath:/static/")
                         .resourceChain(false)
-                        .addResolver(new EncodedResourceResolver())
-                        .addResolver(new GzipResourceResolver());
+                        .addResolver(new EncodedResourceResolver());
             }
         };
     }
