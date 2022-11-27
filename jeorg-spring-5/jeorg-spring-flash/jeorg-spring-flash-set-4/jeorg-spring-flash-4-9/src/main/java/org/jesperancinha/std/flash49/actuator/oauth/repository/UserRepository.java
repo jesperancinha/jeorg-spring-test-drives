758e27017edeac97c4a6caa03c47bf1ba7379922
Joao Esperancinha <jofisaes@gmail.com>
2022-11-27 21:32:20 +0100

    Module 4-9 removed due to old resource server for OAuth having been deprecated

--- jeorg-spring-5/jeorg-spring-flash/jeorg-spring-flash-set-4/jeorg-spring-flash-4-9/src/main/java/org/jesperancinha/std/flash49/actuator/oauth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jesperancinha.std.flash49.actuator.oauth.repository;
-
-import org.jesperancinha.std.flash49.actuator.oauth.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-    User findUserByName(String name);
-}
