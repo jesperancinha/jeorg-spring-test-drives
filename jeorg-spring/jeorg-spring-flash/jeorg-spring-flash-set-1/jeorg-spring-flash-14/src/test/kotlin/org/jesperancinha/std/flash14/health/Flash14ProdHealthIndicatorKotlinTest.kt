afdf5e29d033fefa713868beb35fa09556ceaf36
Joao Esperancinha <jofisaes@gmail.com>
2023-01-23 22:08:03 +0100

    Adds helper plugin to make sure sources are read

---
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-14/src/test/kotlin/org/jesperancinha/std/flash14/health/Flash14ProdHealthIndicatorKotlinTest.kt
@@ -0,0 +1,60 @@
+package org.jesperancinha.std.flash14.health
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.string.shouldContain
+import org.apache.commons.io.IOUtils
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.DefaultResponseErrorHandler
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.RestTemplate
+import java.io.IOException
+import java.nio.charset.Charset
+
+@ActiveProfiles("prod")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = [Flash14ProdHealthIndicator::class])
+@EnableAutoConfiguration
+internal class Flash14ProdHealthIndicatorKotlinTest @Autowired constructor(
+) {
+    private val restTemplate by lazy { RestTemplate() }
+
+    @LocalServerPort
+    private val port: Long? = null
+
+    @Test
+    fun testHealthCheckWhenEndpointCalledThenReturnHealthChecks() {
+        val url = "http://localhost:$port/actuator/health"
+        val headers = HttpHeaders()
+        val request = HttpEntity<Any>(headers)
+        restTemplate.errorHandler = object : DefaultResponseErrorHandler() {
+            @Throws(IOException::class)
+            override fun handleError(response: ClientHttpResponse, statusCode: HttpStatusCode) {
+                IOUtils.toString(response.body, Charset.defaultCharset())
+                    .shouldNotBeNull()
+                    .shouldContain("\"lyrics\":\"Without you what does my life amount to\"")
+                ConsolerizerComposer.outSpace()
+                    .green("All assertions complete!")
+                    .reset()
+                super.handleError(response, statusCode)
+            }
+        }
+        shouldThrow<HttpServerErrorException> {
+            restTemplate.exchange(
+                url, HttpMethod.GET,
+                request, String::class.java
+            )
+        }
+    }
+}
