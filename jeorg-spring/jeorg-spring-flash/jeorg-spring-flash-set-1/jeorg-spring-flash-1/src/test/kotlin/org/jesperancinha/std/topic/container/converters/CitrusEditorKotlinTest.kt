36ee425ecd0afff1270676ea4090ed6520dc99ba
Joao Esperancinha <jofisaes@gmail.com>
2023-01-23 09:34:38 +0100

    Limes and Lemons

--- jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-1/src/test/kotlin/org/jesperancinha/std/topic/container/converters/CitrusEditorKotlinTest.kt
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-1/jeorg-spring-flash-1/src/test/kotlin/org/jesperancinha/std/topic/container/converters/CitrusEditorKotlinTest.kt
@@ -7,11 +7,19 @@ import org.junit.jupiter.api.Test
 
 internal class CitrusEditorKotlinTest {
     @Test
-    fun testSetAsTextWhenSetTextThenTextIsOk() {
+    fun `should get correct Lemon value`() {
         val citrusEditor = CitrusEditor()
         citrusEditor.asText = "lemon-123"
         val value = citrusEditor.value
-       (value as Lemon).size shouldBe 123
         value.shouldBeTypeOf<Lemon>()
+        value.size shouldBe 123
+    }
+    @Test
+    fun `should get correct Lime value`() {
+        val citrusEditor = CitrusEditor()
+        citrusEditor.asText = "lime-124"
+        val value = citrusEditor.value
+        value.shouldBeTypeOf<Lime>()
+        value.size shouldBe 124
     }
 }
