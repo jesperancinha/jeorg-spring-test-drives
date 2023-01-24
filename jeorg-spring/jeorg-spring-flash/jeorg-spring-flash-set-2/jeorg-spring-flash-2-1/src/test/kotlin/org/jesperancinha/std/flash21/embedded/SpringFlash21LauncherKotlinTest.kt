c38a6d3bd1b6e757efe461208a28bd7b2529d1ec
Joao Esperancinha <jofisaes@gmail.com>
2023-01-24 14:26:11 +0100

    Kotests for Flash 2.1

---
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-1/src/test/kotlin/org/jesperancinha/std/flash21/embedded/SpringFlash21LauncherKotlinTest.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.std.flash21.embedded
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.web.WebAppConfiguration
+
+@WebAppConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+internal class SpringFlash21LauncherKotlinTest {
+    @Test
+    fun `should run application context`() {
+    }
+}
---
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-1/src/test/kotlin/org/jesperancinha/std/flash21/embedded/SpringFlash21LauncherRandomPortKotlinTest.kt
@@ -0,0 +1,11 @@
+package org.jesperancinha.std.flash21.embedded
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class SpringFlash21LauncherRandomPortKotlinTest {
+    @Test
+    fun `should run application context`() {
+    }
+}
