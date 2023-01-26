ff089a1ecf08d2a2da37b5c3e2b95b44e60e1d7e
Joao Esperancinha <jofisaes@gmail.com>
2023-01-26 14:55:08 +0100

    Kotests for Flash 2.8

---
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-8/src/test/kotlin/org/jesperancinha/std/flash28/security/SpringFlash28LauncherKotlinTest.kt
@@ -0,0 +1,26 @@
+package org.jesperancinha.std.flash28.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+internal class SpringFlash28LauncherKotlinTest(
+    @Autowired
+    private val mockMvc: MockMvc
+) {
+    @Test
+    @Throws(Exception::class)
+    fun `should fetch bird`() {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk)
+            .andExpect(content().string("A little bird lit down on henry lee"))
+    }
+}
