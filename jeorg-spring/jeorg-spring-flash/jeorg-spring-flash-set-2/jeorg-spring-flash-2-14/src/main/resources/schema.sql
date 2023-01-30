715730a946328b84d612f94705d3f0b447b622eb
Joao Esperancinha <jofisaes@gmail.com>
2023-01-30 11:08:53 +0100

    Adds primary key to test 2-14

--- jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-14/src/main/resources/schema.sql
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-14/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table if not exists car
 (
-    id                SERIAL,
+    id                SERIAL PRIMARY KEY,
     model             varchar(255),
     brand             varchar(255),
     year              int,
