05742d0d1fcb94033a43696a27e300a870d0c143
Joao Esperancinha <jofisaes@gmail.com>
2023-01-30 12:08:30 +0100

    updates car_year column

--- jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-14/src/main/resources/schema.sql
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-14/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ create table if not exists car
     id                SERIAL PRIMARY KEY,
     model             varchar(255),
     brand             varchar(255),
-    carYear           int,
+    car_year          int,
     movie_appearances varchar(255)
 );
