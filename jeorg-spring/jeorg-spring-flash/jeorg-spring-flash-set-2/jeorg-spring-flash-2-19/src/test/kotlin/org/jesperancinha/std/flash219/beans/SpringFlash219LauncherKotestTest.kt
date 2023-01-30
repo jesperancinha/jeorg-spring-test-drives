c44acd6c79e1a5e83ddb3b516e522d1a9a525557
Joao Esperancinha <jofisaes@gmail.com>
2023-01-30 15:49:00 +0100

    Chunk it up for parallelization

--- jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-19/src/test/kotlin/org/jesperancinha/std/flash219/beans/SpringFlash219LauncherKotestTest.kt
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-19/src/test/kotlin/org/jesperancinha/std/flash219/beans/SpringFlash219LauncherKotestTest.kt
@@ -1,11 +1,30 @@
 package org.jesperancinha.std.flash219.beans
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.apache.commons.lang3.RandomStringUtils.random
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
+import java.util.*
 
 @SpringBootTest
 internal class SpringFlash219LauncherKotestTest {
     @Test
-    fun `should initialize application content`() {
+    fun `should initialize application content`(): Unit = runBlocking {
+        listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j").chunked(2).flatMap { chunk ->
+            val chunkId = UUID.randomUUID()
+            chunk.map {
+                async {
+                    delay((Math.random() * 100).toLong())
+                    ConsolerizerComposer.outSpace()
+                        .orange("Chunk $chunkId run at ${LocalDateTime.now()} with letter $it")
+                        .reset()
+                }
+            }
+        }.awaitAll()
     }
 }
