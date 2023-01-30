f692ba6fd755f5504a2250c4b71e486c10842350
Joao Esperancinha <jofisaes@gmail.com>
2023-01-30 13:18:47 +0100

    Kotests for Flash 2.20

---
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-20/src/test/kotlin/org/jesperancinha/std/flash220/allparams/SpringFlash220LauncherKotlinTest.kt
@@ -0,0 +1,11 @@
+package org.jesperancinha.std.flash220.allparams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class SpringFlash220LauncherKotlinTest {
+    @Test
+    fun `should load application context`() {
+    }
+}
