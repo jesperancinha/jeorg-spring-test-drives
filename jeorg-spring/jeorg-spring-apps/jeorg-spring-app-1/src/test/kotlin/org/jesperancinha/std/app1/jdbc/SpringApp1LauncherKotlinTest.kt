f88a83b05db342726405c9dce0b333f5e691da61
Joao Esperancinha <jofisaes@gmail.com>
2023-02-08 00:02:13 +0100

    Refactors code

--- jeorg-spring/jeorg-spring-apps/jeorg-spring-app-1/src/test/kotlin/org/jesperancinha/std/app1/jdbc/SpringApp1LauncherKotlinTest.kt
+++ jeorg-spring/jeorg-spring-apps/jeorg-spring-app-1/src/test/kotlin/org/jesperancinha/std/app1/jdbc/SpringApp1LauncherKotlinTest.kt
@@ -1,12 +1,12 @@
 package org.jesperancinha.std.app1.jdbc
 
-import org.jesperancinha.std.app1.jdbc.containers.AbstractTestContainersKotlinIT
+import org.jesperancinha.std.app1.jdbc.containers.AbstractTestContainersIT
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
-@ContextConfiguration(initializers = [AbstractTestContainersKotlinIT.DockerPostgresDataInitializer::class])
+@ContextConfiguration(initializers = [AbstractTestContainersIT.DockerPostgresDataInitializer::class])
 class SpringApp1LauncherKotlinTest {
     @Test
     fun `should load context`() {
