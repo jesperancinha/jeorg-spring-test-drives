7dae373b8a5d07822d3c6ff5872a7779aa0b5d24
jesperancinha <jofisaes@gmail.com>
2024-01-14 14:18:19 +0100

    Removes unnecessary java docs

--- jeorg-spring/jeorg-spring-app-old/jeorg-spring-test-drives-webapp/src/main/java/org/jesperancinha/std/old/webapp/service/DetailService.java
+++ jeorg-spring/jeorg-spring-app-old/jeorg-spring-test-drives-webapp/src/main/java/org/jesperancinha/std/old/webapp/service/DetailService.java
@@ -5,14 +5,6 @@ import org.jesperancinha.std.old.webapp.repository.DetailRepository;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-
-// TODO: p0 Is placed instead of an id which is what should have been done
-// However the use of #id always resulted in a null which broke the application
-// Fix later, moving on now.
-
-/**
- *
- */
 @Service
 public class DetailService {
 
