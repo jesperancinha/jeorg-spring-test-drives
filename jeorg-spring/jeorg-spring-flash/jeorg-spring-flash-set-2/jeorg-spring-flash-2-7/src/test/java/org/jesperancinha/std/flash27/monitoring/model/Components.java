623a80a324f91eace780a0779745ed64e204574e
jesperancinha <jofisaes@gmail.com>
2024-11-29 14:27:59 +0100

    adds missing property

--- jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-7/src/test/java/org/jesperancinha/std/flash27/monitoring/model/Components.java
+++ jeorg-spring/jeorg-spring-flash/jeorg-spring-flash-set-2/jeorg-spring-flash-2-7/src/test/java/org/jesperancinha/std/flash27/monitoring/model/Components.java
@@ -2,7 +2,9 @@ package org.jesperancinha.std.flash27.monitoring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Components(@JsonProperty("db") DataBaseF27 db,
+public record Components(
+        @JsonProperty("ssl") Object ssl,
+        @JsonProperty("db") DataBaseF27 db,
                          @JsonProperty("diskSpace") DiskSpace diskSpace,
                          @JsonProperty("flash27") Flash27 flash27,
                          @JsonProperty("ping") Ping ping) {
